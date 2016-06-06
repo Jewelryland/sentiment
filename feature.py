f66df378c3443aa72972fde48c4dce079457234c
lostleaf <lostleaf@me.com>
2016-06-06 17:54:05 -0500

    extract and train for multiple features, make it same as the report

--- feature.py
+++ feature.py
@@ -1,5 +1,7 @@
 import numpy as np
 import re
+import cPickle as pickle
+import nltk
 from bs4 import BeautifulSoup
 from glob import glob
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -13,23 +15,20 @@ def read_reviews(path):
             reviews.append(fin.read())
     return reviews
 
-def preproc(review):
+def preproc(review, use_stopwords=False):
     review_text = BeautifulSoup(review, "lxml").get_text()
     review_text = re.sub("[^a-zA-Z]"," ", review_text)
 
-    # stops = set(stopwords.words("english"))
-    # words = [w for w in words if not w in stops]
-
-    # b=[]
-    # stemmer = english_stemmer
-    # for word in words:
-    #     b.append(stemmer.stem(word))
+    if use_stopwords:
+        stops = set(nltk.stopwords.words("english"))
+        words = [w for w in review_text.split() if not w in stops]
+        return " ".join(words)
 
     return review_text.lower()
 
 def read_data(path):
-    pos_reviews = read_reviews(TRAIN_DIR + "/pos")
-    neg_reviews = read_reviews(TRAIN_DIR + "/neg")
+    pos_reviews = read_reviews(path + "/pos")
+    neg_reviews = read_reviews(path + "/neg")
     reviews = pos_reviews + neg_reviews
     labels = np.concatenate((np.ones(len(pos_reviews)), np.zeros(len(neg_reviews))))
     return reviews, labels
@@ -50,23 +49,39 @@ reviews_train = [preproc(review) for review in reviews_train]
 reviews_test = [preproc(review) for review in reviews_test]
 reviews_unsup = [preproc(review) for review in reviews_unsup]
 
-print "extract feature"
+def extract(max_gram, feat_dims, save_model=False):
+    print "extract feature"
 
-vectorizer = TfidfVectorizer( min_df=2, max_df=0.95, max_features = None, ngram_range = ( 1, 4 ),
-                              sublinear_tf = True )
+    vectorizer = TfidfVectorizer( min_df=2, max_df=0.95, max_features=None, 
+            ngram_range=(1, max_gram), sublinear_tf = True )
 
-vectorizer = vectorizer.fit(reviews_train + reviews_unsup)
-feats_train = vectorizer.transform(reviews_train)
-feats_test = vectorizer.transform(reviews_test)
+    vectorizer = vectorizer.fit(reviews_train + reviews_unsup)
+    feats_train_ori = vectorizer.transform(reviews_train)
+    feats_test_ori = vectorizer.transform(reviews_test)
+    print "size of orginal train features", feats_train_ori.shape
 
-print "perform feature selection"
+    for feat_dim in feat_dims:
+        print "perform feature selection"
 
-fselect = SelectKBest(chi2 , k=70000)
-feats_train = fselect.fit_transform(feats_train, labels_train)
-feats_test = fselect.transform(feats_test)
+        fselect = SelectKBest(chi2 , k=feat_dim)
+        feats_train = fselect.fit_transform(feats_train_ori, labels_train)
+        feats_test = fselect.transform(feats_test_ori)
 
-print feats_train.shape, feats_test.shape
-np.save("feats_train.npy", feats_train)
-np.save("feats_test.npy", feats_test)
-np.save("labels_train.npy", labels_train)
-np.save("labels_test.npy", labels_test)
+        print "save features"
+        np.savez("feats/%d_%d.npz" % (max_gram, feat_dim), 
+                feats_train=feats_train, feats_test=feats_test, 
+                labels_train=labels_train, labels_test=labels_test)
+
+        if save_model:
+            print "save models"
+            with open("models/vectorizer_%d.pkl" % max_gram, "wb") as fout:
+                pickle.dump(vectorizer, fout, -1)
+
+            with open("models/fselect_%d_%d.pkl" % (max_gram, feat_dim), "wb") as fout:
+                pickle.dump(fselect, fout, -1)
+
+# extract(4, [1000, 3000, 5000, 10000, 30000, 70000, 100000])
+# extract(3, [1000, 3000, 5000, 10000, 30000, 70000, 100000])
+# extract(2, [1000, 3000, 5000, 10000, 30000, 70000, 100000])
+# extract(1, [1000, 3000, 5000, 10000, 30000, 70000, 100000])
+extract(3, [30000])
--- train.py
+++ train.py
@@ -1,13 +1,21 @@
 import numpy as np
+from glob import glob
 from sklearn.linear_model import LogisticRegression as LogRegr
+from sklearn.grid_search import GridSearchCV
 
-print "Training..."
+for path in glob("feats/*.npz"):
+    name = path.split("/")[-1].split(".")[0]
+    max_gram, dim = name.split("_")
+    print "Loading 1-%s gram, feature dimension %s" % (max_gram, dim)
+    with open(path, "rb") as fin:
+        data = np.load(fin)
+        feats_train = data["feats_train"].item(0)
+        feats_test = data["feats_test"].item(0)
+        labels_train = data["labels_train"]
+        labels_test = data["labels_test"]
 
-feats_train = np.load("feats_train.npy").item(0)
-feats_test = np.load("feats_test.npy").item(0)
-labels_train = np.load("labels_train.npy")
-labels_test = np.load("labels_test.npy")
-
-clf = LogRegr()
-clf.fit(feats_train, labels_train)
-print clf.score(feats_test, labels_test)
+    print "Training..."
+    c_grid = 10.0 ** np.arange(-5, 6)
+    clf = GridSearchCV(LogRegr(), {'C':c_grid}, n_jobs=-1, cv=5)
+    clf.fit(feats_train, labels_train)
+    print "train accuracy %f, test accuracy %f" % (clf.score(feats_train, labels_train), clf.score(feats_test, labels_test))
